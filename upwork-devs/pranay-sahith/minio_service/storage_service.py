24d6f82d9e4fdd27db1c4e8a3116aa4ecb5e2176
pranaysahith <pranaysahith7@gmail.com>
2020-09-15 11:56:28 +0530

    update storage service

--- upwork-devs/pranay-sahith/minio_service/storage_service.py
+++ upwork-devs/pranay-sahith/minio_service/storage_service.py
@@ -7,8 +7,8 @@ from minio_service import MinioService
 class StorageService(MinioService):
 
     def __init__(self, url, access_key, secret_key):
-        url = url
-        access_key = access_key
-        secret_key = secret_key
+        self.url = url
+        self.access_key = access_key
+        self.secret_key = secret_key
         super().__init__(url, access_key, secret_key)
 
